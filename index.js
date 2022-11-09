1c03c9148590f741ecb69dbf9a4ea2c5ff1ae4eb
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-11-09 22:46:02 +0600

    removed fake data and mongo db is used to get services

--- index.js
+++ index.js
@@ -4,48 +4,35 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
-require('dotenv').config();
 
-
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ct9it9z.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ct9it9z.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-  
 });
 
-async function run(){
-  try{
-    const serviceCollection = client.db('tommy-resturant').collection('services');
-    app.get('/services', async(req, res)=>{
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("tommy-resturant")
+      .collection("allServices");
+
+    app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-      const services = cursor.toArray();
-      res.send();
-    })
-
-  }
-  finally{
-
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+  } finally {
   }
 }
-run().catch((err)=>{console.log('error: ', err)})
-
-
-
-const services = require('./data/services.json');
-
-app.get("/services", (req, res) => {
-  
-  res.send(services);
-});
+run().catch((err) => console.log("Error: ", err));
 
 app.listen(port, () => {
-  console.log(`mongod db server is running, ${port}`);
-  console.log(uri);
+  console.log(`mongodb db server is running, ${port}`);
 });
