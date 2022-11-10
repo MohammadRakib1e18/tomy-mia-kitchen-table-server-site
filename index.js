83f58b2dcd200bb3b707b4f29a556287647e4901
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-11-10 06:52:35 +0600

    single service api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
@@ -34,6 +34,12 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    })
   } finally {
   }
 }
