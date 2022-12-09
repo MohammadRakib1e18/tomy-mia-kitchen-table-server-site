33b47cdbe584cda5c6b4ec3d8ae86c6b4906c140
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-12-09 12:51:32 +0600

    order api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
@@ -16,6 +17,22 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized author1" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access2" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     const serviceCollection = client
@@ -24,6 +41,16 @@ async function run() {
     const reviewCollection = client
       .db("tommy-resturant")
       .collection("allReviews");
+    const orderCollection = client
+      .db("tommy-resturant")
+      .collection("orders");
+    
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN);
+      res.send({ token });
+    });
 
     app.get("/services", async (req, res) => {
       const query = {};
@@ -43,14 +70,20 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
-    app.get("/allReviews", async (req, res) => {
+
+    app.get("/allReviews", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+
       let query = {};
       if (req?.query?.email) {
+        if (decoded.email !== req.query.email) {
+          res.status(403).send({ message: "unauthorized access3" });
+        }
         query = {
           email: req.query.email,
         };
       }
-      const cursor = reviewCollection.find(query).sort({total_time:-1});
+      const cursor = reviewCollection.find(query).sort({ total_time: -1 });
       const reviews = await cursor.toArray();
       res.send(reviews);
     });
@@ -61,28 +94,39 @@ async function run() {
       res.send(review);
     });
 
+    app.get('/order/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {product_id : id};
+      const order = await orderCollection.findOne(query);
+      console.log(order);
+      res.send(order);
+    })
+
+    app.post('/order', async(req, res)=>{
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    })
+
     app.post("/addReview", async (req, res) => {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
       res.send(result);
-      console.log(result);
     });
     app.post("/addService", async (req, res) => {
       const service = req.body;
       console.log(service);
       const result = await serviceCollection.insertOne(service);
       res.send(result);
-      console.log(result);
     });
     app.put("/allReviews/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const option = { upsert: true };
       const review = req.body;
-      console.log(review);
       const updatedReview = {
         $set: {
-          message: review.message
+          message: review.message,
         },
       };
       const result = await reviewCollection.updateOne(
@@ -90,15 +134,12 @@ async function run() {
         updatedReview,
         option
       );
-      console.log(result);
       res.send(result);
     });
     app.delete("/services/delete/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: ObjectId(id) };
       const result = await reviewCollection.deleteOne(query);
-      console.log(result);
       res.send(result);
     });
   } finally {
